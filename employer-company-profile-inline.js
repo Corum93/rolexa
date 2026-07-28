1d657f6dc0d2e6e18ac7ebb653bc9cbbe94cd473
Corum93 <corumcain93@gmail.com>
2026-07-28 11:04:42 +0100

    Load employer application activity timeline

--- employer-company-profile-inline.js
+++ employer-company-profile-inline.js
@@ -96,4 +96,5 @@
   loadOnce('employer-verification-overview-warning.js?v=1', 'data-rx-verification-overview-warning');
   loadOnce('employer-hiring-stages.js?v=1', 'data-rx-employer-hiring-stages');
   loadOnce('employer-accountability-overview.js?v=4', 'data-rx-employer-accountability-overview');
+  loadOnce('employer-application-activity.js?v=1', 'data-rx-employer-application-activity');
 })();
